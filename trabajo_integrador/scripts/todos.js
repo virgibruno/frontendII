d0a786b95ca8a22f7153d8e96bfabd1b643de426
virgibruno <virgi.bruno1995@gmail.com>
2021-06-28 21:33:48 -0300

    marcar tarea como finalizada - 1ra aproximacion

--- trabajo_integrador/scripts/todos.js
+++ trabajo_integrador/scripts/todos.js
@@ -1,6 +1,3 @@
-window.addEventListener("load", ()=>{
-    renderizarTodos()
-})
 
 let listadoTodos = [
     {
@@ -11,7 +8,7 @@ let listadoTodos = [
     {
         description: "Mi hermosa tarea 2",
         createdAt: "19/04/20",
-        pendiente: true,
+        pendiente: false,
     },
     {
         description: "Mi hermosa tarea 3",
@@ -32,67 +29,84 @@ let listadoTodos = [
 
 let nuevaTarea = (tarea) => {
     return `
-     <li class="tarea">
-      <div class="not-done"></div>
-      <div class="descripcion">
-        <p class="nombre">${tarea.description}</p>
-        <p class="timestamp">Creada: ${tarea.createdAt}</p>
-      </div>
+    <li class="tarea">
+        <div class="not-done"></div>
+        <div class="descripcion">
+            <p class="nombre">${tarea.description}</p>
+            <p class="timestamp">Creada: ${tarea.createdAt}</p>
+        </div>
     </li> 
     `
 }
 
-const tareasPendientes = document.querySelector('.tareas-pendientes');
-const tareasTerminadas = document.querySelector('.tareas-terminadas');
+window.addEventListener("load", ()=>{
+    const tareasPendientes = document.querySelector(".tareas-pendientes");
+    const tareasTerminadas = document.querySelector(".tareas-terminadas");
+    const enviarNvaTarea = document.querySelector("form.nueva-tarea")
+    
+    renderizarTodos()
 
 
-function renderizarTodos() {
-    tareasPendientes.innerHTML = "";
-    tareasTerminadas.innerHTML = "";
+    function renderizarTodos() {
+        tareasPendientes.innerHTML = "";
+        tareasTerminadas.innerHTML = "";
 
-    for (tarea of listadoTodos) {
-        if (tarea.pendiente) {
-            tareasPendientes.innerHTML += nuevaTarea(tarea)
-        } else {
-            tareasTerminadas.innerHTML += nuevaTarea(tarea)
+        for (tarea of listadoTodos) {
+            if (tarea.pendiente) {
+                tareasPendientes.innerHTML += nuevaTarea(tarea)
+            } else {
+                tareasTerminadas.innerHTML += nuevaTarea(tarea)
+            }
         }
-    }
-}
 
+        /* -------------------------------------------
+            Marcar tarea como finalizada
+        ------------------------------------------- */
+        const btnsMarcarFin = document.querySelectorAll(".not-done")
+        
+        for (btn of btnsMarcarFin) {
+            btn.addEventListener("click", e => {
+                let tareaParaMarcar = e.srcElement.nextElementSibling
+                console.log(tareaParaMarcar.innerHTML)
+                for (tarea of listadoTodos) {
+                    if (tarea.description == tareaParaMarcar){
 
-/* -------------------------------------------
-    Crear nueva tarea  
-------------------------------------------- */
-
-function fechaDeHoy() {
-    let today = new Date();
-    const dateOptions = {
-        day: "numeric",
-        month: "numeric",
-        year: "2-digit"
-    }
-    return today.toLocaleDateString("es-AR", dateOptions)
-}
-
-const enviarNvaTarea = document.querySelector("form.nueva-tarea")
-
-enviarNvaTarea.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let NvaTareaText = document.querySelector("#textNuevaTarea").value;
-    if (NvaTareaText !="") {
-        let NvaTarea = {
-            description: document.querySelector("#textNuevaTarea").value,
-            createdAt: fechaDeHoy(),
-            pendiente: true,
+                    }
+                }
+            })
         }
-        listadoTodos.unshift(NvaTarea);
-        renderizarTodos();
-        // document.querySelector("#textNuevaTarea").value = "";
-        enviarNvaTarea.reset()
-    };
+
+    }
+    
+
+
+    /* -------------------------------------------
+        Crear nueva tarea  
+    ------------------------------------------- */
+
+    function fechaDeHoy() {
+        let today = new Date();
+        const dateOptions = {
+            day: "numeric",
+            month: "numeric",
+            year: "2-digit"
+        }
+        return today.toLocaleDateString("es-AR", dateOptions)
+    }
+
+    enviarNvaTarea.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let NvaTareaText = document.querySelector("#textNuevaTarea").value;
+        if (NvaTareaText !="") {
+            let NvaTarea = {
+                description: NvaTareaText,
+                createdAt: fechaDeHoy(),
+                pendiente: true,
+            }
+            listadoTodos.unshift(NvaTarea);
+            renderizarTodos();
+            enviarNvaTarea.reset()
+        };
+    })
+
 })
-
-/* -------------------------------------------
-    Marcar tarea como finalizada
-------------------------------------------- */
-
